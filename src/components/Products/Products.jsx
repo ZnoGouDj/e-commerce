e74dbdb881636170bf149e3b0b85b1f072c3b028
ZnoGouDj <znogoud3@gmail.com>
2022-01-17 19:01:41 +0300

    Commerce products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,30 +4,13 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-  {
-    id: 1,
-    name: 'Shoes',
-    description: 'Running shoes.',
-    price: '$5',
-    image: 'https://footwearnews.com/wp-content/uploads/2021/08/nike-zoomx-vaporfly-next.jpg',
-  },
-  {
-    id: 2,
-    name: 'Macbook',
-    description: 'Apple macbook.',
-    price: '$10',
-    image: 'https://www.iguides.ru/upload/medialibrary/f1e/f1ee0da4d4d5e10c3ad34eaf80960e72.jpg',
-  },
-];
-
-const Products = () => {
+const Products = ({ products }) => {
   const classes = useStyles();
 
   return (
     <main className={classes.content}>
       <div className={classes.toolbar} />
-      <Grid container justify="center" spacing={4}>
+      <Grid container justifyContent="center" spacing={4}>
         {products.map(product => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
             <Product product={product} />
