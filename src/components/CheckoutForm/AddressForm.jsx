0b25243378148574190746c688ada48787dc92bc
ZnoGouDj <znogoud3@gmail.com>
2022-01-18 18:33:02 +0300

    Fetch Countries

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,19 +1,33 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
 import { useForm, FormProvider } from 'react-hook-form';
 
-import { commerce } from '../../lib/commerce'
+import { commerce } from '../../lib/commerce';
 
 import FormInput from './CustomTextField';
 
-const AddressForm = () => {
-  const [shippingCountries, setShippingCountries] = useState([])
-  const [shippingCountry, setShippingCountry] = useState('')
-  const [shippingSubdevisions, setShippingSubdevisions] = useState([])
-  const [shippingSubdevision, setShippingSubdevision] = useState('')
-  const [shippingOptions, setShippingOptions] = useState([])
-  const [shippingOption, setShippingOption] = useState('')
+const AddressForm = ({ checkoutToken }) => {
+  const [shippingCountries, setShippingCountries] = useState([]);
+  const [shippingCountry, setShippingCountry] = useState('');
+  const [shippingSubdevisions, setShippingSubdevisions] = useState([]);
+  const [shippingSubdevision, setShippingSubdevision] = useState('');
+  const [shippingOptions, setShippingOptions] = useState([]);
+  const [shippingOption, setShippingOption] = useState('');
   const methods = useForm();
+
+  const countries = Object.entries(shippingCountries).map(([code, name]) => ({ id: code, label: name }));
+  console.log(countries);
+  const fetchShippingCountries = async checkoutTokenId => {
+    const { countries } = await commerce.services.localeListShippingCountries(checkoutTokenId);
+
+    console.log(countries);
+    setShippingCountries(countries);
+    setShippingCountry(Object.keys(countries)[0]);
+  };
+
+  useEffect(() => {
+    fetchShippingCountries(checkoutToken.id);
+  }, []);
 
   return (
     <>
@@ -29,13 +43,15 @@ const AddressForm = () => {
             <FormInput required name="zip" label="VIP / Postal code" />
             <Grid item xs={12} sm={6}>
               <InputLabel>Shipping Country</InputLabel>
-              <Select value={ } fullWidth onChange={}>
-                <MenuItem key={} value={}>
-                  Select Me
-                </MenuItem>
+              <Select value={shippingCountry} fullWidth onChange={e => setShippingCountry(e.target.value)}>
+                {countries.map(country => (
+                  <MenuItem key={country.id} value={country.id}>
+                    {country.label}
+                  </MenuItem>
+                ))}
               </Select>
             </Grid>
-            <Grid item xs={12} sm={6}>
+            {/* <Grid item xs={12} sm={6}>
               <InputLabel>Shipping Subdivision</InputLabel>
               <Select value={ } fullWidth onChange={}>
                 <MenuItem key={} value={}>
@@ -49,8 +65,8 @@ const AddressForm = () => {
                 <MenuItem key={} value={}>
                   Select Me
                 </MenuItem>
-              </Select>
-            </Grid>
+              </Select> */}
+            {/* </Grid> */}
           </Grid>
         </form>
       </FormProvider>
